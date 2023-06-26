6f37a26f15578385c27d9a96e3556e065385040a
Danylo Vovchok <vovchok.danya01@gmail.com>
2023-06-26 17:22:16 +0200

    refactoring1(add item strategies classes for all the strategies)

--- gilded_rose/refactored/gilded_rose.py
+++ gilded_rose/refactored/gilded_rose.py
@@ -35,6 +35,33 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
+class RegularItemStrategy:
+    def update_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1                    
+
+class AgedBrieItemStrategy:
+    def update_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+class BackstagePassItemStrategy:
+    def update_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
 
 class Item:
     def __init__(self, name, sell_in, quality):

c9008e4c7ff3ebd58f77aeeed35847838aba3c9b
martinbc24 <116806411+mar7-n@users.noreply.github.com>
2023-06-26 18:36:15 +0200

    sulfuras_test.py hinzugefügt

---
+++ tests/sulfuras_test.py
@@ -0,0 +1,16 @@
+from tests.settings import *
+
+def test_sulfuras_normal():
+    item = Item("Sulfuras, Hand of Ragnaros", 10, 80)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 10
+    assert item.quality == 80
+
+@pytest.mark.xfail(xfail_bug_in_original, reason = "Sulfuras' quality is always 80")
+def test_sulfuras_quality():
+    item = Item("Sulfuras, Hand of Ragnaros", 23, 70)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.sell_in == 23
+    assert item.quality == 70
